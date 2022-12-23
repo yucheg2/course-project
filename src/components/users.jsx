8b26aa85f5db99b0cdf330c6e7691c14ab56a639
yucheg2 <prostoyurka1223@gmail.com>
2022-12-23 06:12:33 +0300

    make-table

---
+++ src/components/users.jsx
@@ -0,0 +1,73 @@
+import React,{useState} from "react";
+import api from "../API/index"
+
+const Users = () => {
+    const [users, setUsers] = useState(api.users.fetchAll())
+
+    const handleDelete = (userId) => {
+        setUsers ((prevState)=>prevState.filter((user)=> user._id !== userId))        
+    }
+
+    const renderQualities = (qualities) => {
+        return qualities.map((qualiti) => {
+            return (<span className={`badge m-1 text-bg-${qualiti.color}`}>{qualiti.name}</span>)
+        })
+    }
+    const renderPhrase = (number) => {
+        let phrase = `${number} `
+        if (number >4 || number === 1) {
+            phrase += 'человек тусанет с тобой сегодня'
+        } else if (number <= 4 && number >1) {
+            phrase += 'человека тусанут с тобой сегодня'
+        } else {
+            phrase = 'Никто с тобой не тусанет'
+        }
+        return phrase
+    }
+    const getPhraseClass = () => {
+       let classes = 'badge '
+       return classes += users.length !== 0 ? 'text-bg-primary' : 'text-bg-danger'
+    }
+
+    const renderTable = () => {
+        if(users.length !== 0){
+            return (
+            <table className="table">
+                <thead>
+                <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Провфессия</th>
+                <th scope="col">Встретился раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, id) => {
+                        return (
+                            <tr>
+                                <td>{user.name}</td>  
+                                <td>{renderQualities(user.qualities)}</td> 
+                                <td>{user.profession.name}</td>
+                                <td>{user.completedMeetings}</td>
+                                <td>{`${user.rate}/5`}</td>
+                                <td><button onClick={()=>handleDelete(user._id)} className="btn btn-danger">delete</button></td>
+                            </tr>
+                            )
+                    })}
+                </tbody>
+            </table>)
+            }
+    }
+    return (
+        <>
+            <h2>
+                <span className={getPhraseClass()}>{renderPhrase(users.length)}</span>
+            </h2>
+            {renderTable()}
+        </>
+    )
+}
+
+export default Users
